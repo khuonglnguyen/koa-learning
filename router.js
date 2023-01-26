812f0e7233fa589f89019fe32106be839ebd3b4f
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-26 17:42:19 +0700

    Add download file

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const Router = require("koa-router");
 const router = new Router();
 const { getEvents, postEvent } = require("./controllers/events.controllers");
-const { getPhotos, postPhotos } = require("./controllers/photos.controllers");
+const { getPhotos, postPhotos, downloadPhotos } = require("./controllers/photos.controllers");
 const { cryptoListCache } = require("./middleware/crypto.cache");
 const { getCryptoList } = require("./controllers/crypto.controllers");
 
@@ -9,6 +9,7 @@ router.get("/crypto_list", cryptoListCache, getCryptoList);
 router.get("/events_list", getEvents);
 router.get("/photos_list", getPhotos);
 router.post("/post_photo", postPhotos);
+router.post("/download_photo", downloadPhotos);
 router.post("/post_event", postEvent);
 
 module.exports = router;
